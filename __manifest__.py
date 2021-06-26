ad57b3cc401002f6a537d8c61b857dbe13bb9c54
Long <lqlongvn@gmail.com>
2021-06-26 10:01:34 +0700

    qlms(26-6-21-9h58)

---
+++ __manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Module_1',
+    'version': '1.1',
+    'summary': 'Đồ án của Long',
+    'sequence': 3,
+    'author': 'ITPlus, Long',
+    'description': """
+        8/6/21 Long khởi tạo theo hướng dẫn của thầy Minh
+    """,
+    'category': 'Other',
+    'website': '',
+    'depends': [],
+    'data': [
+        'views/goithau_view.xml',
+        'views/user_view.xml',
+        'views/donvi_view.xml',
+        'views/thongtin_dauthau_view.xml',
+        'security/security.xml',
+        'security/ir.model.access.csv'
+    ],
+    'installable': True,
+    'application': True,
+}
