f20c2ddefc86b592c2fc9fe1fb20707aad998634
Long <lqlongvn@gmail.com>
2021-07-14 16:49:58 +0700

    Làm sạch code liên quan đến đếm số lượng User và Gói thầu thuộc đơn vị

--- models/donvi.py
+++ models/donvi.py
@@ -3,36 +3,24 @@ from odoo import models, fields, api
 class Donvi(models.Model):
     _name = 'donvi'
 
-
     name = fields.Char(string='Tên rút gọn của Đơn vị')
     # ten_rutgon_donvi = fields.Char(string='Tên rút gọn của Đơn vị')
     ma_donvi = fields.Integer(string='Mã Đơn vị')
     ten_daydu_donvi = fields.Char(string='Tên đầy đủ  của Đơn vị')
 
-    user_ids = fields.One2many(comodel_name='tbl_user', inverse_name='donvi_id', string='User')
-    goithau_ids = fields.One2many(comodel_name='goithau', inverse_name='donvi_id', string='Gói thầu')
-
     def button_donvi(self):
         pass
 
+    goithau_ids = fields.One2many(comodel_name='goithau', inverse_name='donvi_id', string='Gói thầu')
     goithau_count = fields.Integer(compute='get_goithau_count', string='Gói Thầu ', store=False)
-
-    # def get_goithau_count(self):
-    #     self.goithau_count = 8
-    #     return self.goithau_count
-
     @api.depends('goithau_ids')
     def get_goithau_count(self):
         for donvi in self:
             self.goithau_count = len(donvi.goithau_ids)
         return self.goithau_count
 
+    user_ids = fields.One2many(comodel_name='tbl_user', inverse_name='donvi_id', string='User')
     tbluser_count = fields.Integer(compute='get_tbluser_count', string='User ', store=False)
-
-    # def get_tbluser_count(self):
-    #     self.tbluser_count = 6
-    #     return self.tbluser_count
-
     @api.depends('user_ids')
     def get_tbluser_count(self):
         for donvi in self:
