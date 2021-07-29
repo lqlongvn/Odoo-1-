7bfe047dd7142739080eacc759b5ef35bf4823e8
Long <lqlongvn@gmail.com>
2021-07-29 12:02:08 +0700

    Bước 1: trong wizard, thêm 1 button và 1 input text

--- wizard/goithau_remove_reason.py
+++ wizard/goithau_remove_reason.py
@@ -1,4 +1,11 @@
 from odoo import fields, models, api
 
+
 class GoithauRemoveReason(models.TransientModel):
     _name = 'goithau.remove.reason'
+
+    goithau_remove_reason = fields.Selection(
+        selection=[('1', 'Thông tin khai báo sai/thiếu'), ('2', 'Gói thầu chưa được cấp vốn'), ('3', 'Gói thầu đã quá thời hạn triển khai')], string='Reason')
+
+    def update_goithau_remove_reason(self):
+        pass
